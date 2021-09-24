7b924656dc86da7e66a6fdd2049d5dd5f7c3ab1d
Sani J <binabdjalal@gmail.com>
2021-09-24 11:07:20 +0800

    Initial Commit

---
+++ CountryList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryList
+//
+//  Created by Abd Sani Abd Jalal on 24/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
